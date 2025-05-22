8592bc872063ca898c4b92d222b7dc7cae613b25
Kimberly Villatoro <kimsvillatoro@gmail.com>
2025-05-22 17:59:10 -0400

    fix blank page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
             <p className={styles.description}>I am a Full Stack Developer and UX Designer with 5 years developing my tech stack.</p>
             <a href="mailto:kimsvillatoro@gmail.com" className={styles.contactBtn}>Contact Me</a>
         </div>
-        <img src="/assets/hero/heroImage.png" alt="hero-image" className={styles.heroImg} />
+        <img src="assets/hero/heroImage.png" alt="hero-image" className={styles.heroImg} />
         <div className={styles.bottomBlur}/>
     </section>
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,8 +17,8 @@ export const Navbar = () => {
                 className={styles.menuBtn}
                 src= {
                     menuOpen
-                    ? "/assets/nav/closeIcon.png"
-                    : "/assets/nav/menuIcon.png"
+                    ? "assets/nav/closeIcon.png"
+                    : "assets/nav/menuIcon.png"
                 }
                 alt="menu-button"
                 onClick={() => setMenuOpen(!menuOpen)}

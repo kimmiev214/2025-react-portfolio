1c45391fe853e25d89b7bf0e6461246b822d3d49
Kimberly Villatoro <kimsvillatoro@gmail.com>
2025-05-22 17:10:13 -0400

    update base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "2025-react-portfolio/",
   css: {
     modules: {
       localsConvention: "camelCase",
